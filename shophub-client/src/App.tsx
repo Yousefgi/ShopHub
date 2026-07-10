e2144898f267422edfc323d6a685a7b9b0a13ad2
yousef salman <ysalman@itgsoftware.com>
2026-07-10 16:35:36 +0300

    feat(frontend): initialize React app with Vite, TypeScript, and Tailwind CSS

---
+++ shophub-client/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        ShopHub
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+
